3876d9de2891cdb5d513b902edfe6d9a5cb40aa8
RaunaKSharma-err <progritik@gmail.com>
2025-10-06 09:26:08 +0545

    final commit

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -14,7 +14,8 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "http://localhost:8080", credentials: true }));
+app.use(cors({ origin: "*" }));
+
 
 app.get("/", (req, res) => res.json("running"));
 app.use("/api", aqi);
