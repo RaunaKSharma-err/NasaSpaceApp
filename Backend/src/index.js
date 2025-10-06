485328a82c5ca414cb662013f5742767e83e344f
RaunaKSharma-err <progritik@gmail.com>
2025-10-06 09:14:29 +0545

    final commit

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -3,32 +3,28 @@ const dotenv = require("dotenv");
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const PORT = process.env.PORT;
-const _dirname = path.resolve();
 
 dotenv.config();
 
+const PORT = process.env.PORT || 10000;
+const __dirnameResolved = path.resolve();
 const aqi = require("./routes/aqi.routes");
 const app = express();
 
-// CORS
-
-// Body parser
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: "http://localhost:8080", credentials: true }));
 
-// API routes
 app.get("/", (req, res) => res.json("running"));
 app.use("/api", aqi);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(_dirname, "../Client/dist")));
+  app.use(express.static(path.join(__dirnameResolved, "../Client/dist")));
 
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(_dirname, "../Client", "dist", "index.html"));
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirnameResolved, "../Client/dist/index.html"));
   });
 }
 
-// Start server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
